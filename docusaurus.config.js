6b66748584eeb42b2d74ea69a05f4a67e416e963
DanRoscigno <dan@roscigno.com>
2022-12-02 16:33:52 -0500

    zh, not cz

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ru', 'cz'],
+    locales: ['en', 'ru', 'zh'],
   },
 
   presets: [
